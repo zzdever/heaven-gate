0145e8a0e4a28f513c78345c7ffe3a7c03111025
zzdever <zzdever@gmail.com>
2014-12-05 19:27:39 +0800

    before collision

--- collision.cpp
+++ collision.cpp
@@ -6,9 +6,10 @@ collision::collision()
 
 
 
-
+/*
 collision(objectList, eye, eye_center);
 Dimension3f dimension = objectList.at(0)->GetDimension();
 dimension.length;
 Point3f point = objectList.at(0)->GetPosition();
 point.x;
+*/
